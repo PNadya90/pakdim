f8807c684a5400f72f5ec939413b77ad77e29cf1
Nadya <pakusha90@mail.ru>
2022-05-25 12:09:02 +0300

    css html portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-
+  summary=true;
+  iconColorAbout='#FF5D5D';
+  iconColorPortfolio='rgb(121, 110, 110)';
+  showPortfolio(){
+    this.summary=!this.summary;
+    if(this.iconColorAbout=='#FF5D5D'){
+      this.iconColorAbout='rgb(121, 110, 110)';
+      this.iconColorPortfolio='#FF5D5D';
+    }
+   else{
+     this.iconColorAbout='#FF5D5D';
+     this.iconColorPortfolio='rgb(121, 110, 110)';
+   }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TileViewComponent } from './tile-view/tile-view.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppEventList} from './event-list/event-list.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { AboutMeComponent } from './about-me/about-me.component';
     CsDropdownComponent,
     TileViewComponent,
     AppEventList,
-    AboutMeComponent
+    AboutMeComponent,
+    PortfolioComponent
   ],
 
   imports: [
