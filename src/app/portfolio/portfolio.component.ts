46b480d65c4447c0dcfa418e3c471691a75ea116
Nadya <pakusha90@mail.ru>
2022-09-21 20:17:53 +0300

    added media for portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -12,10 +12,7 @@ export class PortfolioComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  goto(url:any, absolute?:boolean){
-    if(absolute){
-      window.window.location.href = url;
-    }
-    this.router.navigateByUrl(url);  
+  goto(url:any){
+    window.open(url, '_blank');
   }
 }
